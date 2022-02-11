ec9313b82ac83205767d65d8c8a978afdab8a2de
Nathan K <njkotzur@hotmail.com>
2022-02-11 17:03:03 +1000

    Mods for breathing machine
    Change to forward direction only
    Add settings for maximum and minimum output percentage

--- IBT-2_SC/src/main.cpp
+++ IBT-2_SC/src/main.cpp
@@ -5,47 +5,36 @@ Speed and direction controlled by a potentiometer attached to analog input 0.
 One side pin of the potentiometer (either one) to ground; the other side pin to +5V
  
 Connection to the IBT-2 board:
-IBT-2 pin 1 (RPWM) to Arduino pin 5(PWM)
-IBT-2 pin 2 (LPWM) to Arduino pin 6(PWM)
+IBT-2 pin 1 (RPWM) to Arduino pin 5(PWM) FORWARD direction
 IBT-2 pins 3 (R_EN), 4 (L_EN), 7 (VCC) to Arduino 5V pin
 IBT-2 pin 8 (GND) to Arduino GND
-IBT-2 pins 5 (R_IS) and 6 (L_IS) not connected
+IBT-2 pins 2 (LPWM), 5 (R_IS) and 6 (L_IS) not connected
 */
 
 #include <Arduino.h>
 
+// Minimum and maximum output speeds
+// Specified in the range 0 - 255
+float minSpeed = 50.0;
+float maxSpeed = 110.0;
+
 int SENSOR_PIN = 0; // center pin of the potentiometer
- 
-int RPWM_Output = 5; // Arduino PWM output pin 5; connect to IBT-2 pin 1 (RPWM)
-int LPWM_Output = 6; // Arduino PWM output pin 6; connect to IBT-2 pin 2 (LPWM)
+int Fwd_PWM_Output = 5; // Arduino PWM output pin 5; connect to IBT-2 pin 1 (RPWM)
 
 
 void setup() {
   // put your setup code here, to run once:
-    pinMode(RPWM_Output, OUTPUT);
-  pinMode(LPWM_Output, OUTPUT);
+  pinMode(Fwd_PWM_Output, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    int sensorValue = analogRead(SENSOR_PIN);
+  int sensorValue = (analogRead(SENSOR_PIN));
  
   // sensor value is in the range 0 to 1023
-  // the lower half of it we use for reverse rotation; the upper half for forward rotation
-  if (sensorValue < 512)
-  {
-    // reverse rotation
-    int reversePWM = -(sensorValue - 511) / 2;
-    analogWrite(LPWM_Output, 0);
-    analogWrite(RPWM_Output, reversePWM);
-  }
-  else
-  {
-    // forward rotation
-    int forwardPWM = (sensorValue - 512) / 2;
-    analogWrite(RPWM_Output, 0);
-    analogWrite(LPWM_Output, forwardPWM);
-  }
-
+  // PWM value is 0 - 255
+  // forward rotation
+  int forwardPWM = sensorValue * (maxSpeed-minSpeed) / 1024 + minSpeed;
+  analogWrite(Fwd_PWM_Output, forwardPWM);
 }
